d00211cd483c88de310545264c34a5e735d2a6e3
innovarew <48243982+innovarew@users.noreply.github.com>
2019-10-19 11:35:25 +0200

    teamviewer-support.bat

---
+++ README.md
@@ -0,0 +1,18 @@
+
+### teamviewer-support: A handy script for remote technical support using TeamViewer
+
+- Download file (Raw > Save as) [teamviewer-support.bat](teamviewer-support.bat).
+- Run (double-click) the downloaded file [teamviewer-support.bat](teamviewer-support.bat) to start TeamViewer.
+
+#### Usage
+
+~~~
+C:\> teamviewer-support.bat
+[*]
+[*] Starting Remote Technical Support . . .
+[*]
+Press any key to continue ...
+
+[After a few seconds TeamViewer will start]
+~~~
+
---
+++ teamviewer-support.bat
@@ -0,0 +1,43 @@
+::
+:: @file teamviewer-support.bat
+:: @version v1.1
+:: @description
+::  A handy script for remote technical support using TeamViewer
+::  Usage: Download & Run to start a TeamViewer remote connection
+::
+:: @url https://innovarew.appspot.com/
+:: @author Innovarew <innovarew.solutions (at) gmail (dot) com>
+:: @copyright (C) 2018 Innovarew Solutions. All Rights Reserved.
+:: @license Proprietary and confidential
+::     Unauthorized copying of this file, via any medium is strictly prohibited.
+:: Contact us if you are interested in using our products and services.
+::
+:: Changelog:
+::
+:: [dd/mm/yyyy]        [author]
+:: [brief description]
+::
+:: 26/12/2018        innovarew
+:: Initial version
+::
+
+@echo off
+
+:: Prevent double-click execution
+if %0 == "%~0" (
+    echo [*]
+    echo [*] Starting Remote Technical Support . . .
+    echo [*]
+    pause
+)
+
+set cwd=%~dp0
+set tv_dir=%cwd%\TeamViewerPortable
+set tv_file=%cwd%\TeamViewerPortable.zip
+set tv_url=https://download.teamviewer.com/download/TeamViewerPortable.zip
+
+cd %cwd%
+ping download.teamviewer.com -n 1
+powershell -command "& { if (!(Test-Path %tv_dir%)) { Invoke-WebRequest %tv_url% -OutFile %tv_file%; Expand-Archive %tv_file% -DestinationPath %tv_dir%; } }"
+
+%tv_dir%\TeamViewer.exe
