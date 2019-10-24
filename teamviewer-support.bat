a18ad835e97cec78c312fc2d9891f5a2a27bf019
innovarew <48243982+innovarew@users.noreply.github.com>
2019-10-24 14:14:04 +0200

    teamviewer-support.bat

--- teamviewer-support.bat
+++ teamviewer-support.bat
@@ -26,18 +26,25 @@
 :: Prevent double-click execution
 if %0 == "%~0" (
     echo [*]
-    echo [*] Starting Remote Technical Support . . .
+    echo [*] [TeamViewer] Starting Remote Technical Support . . .
     echo [*]
     pause
 )
 
 set cwd=%~dp0
+set tv_bat=%cwd%\teamviewer-support.bat
 set tv_dir=%cwd%\TeamViewerPortable
 set tv_file=%cwd%\TeamViewerPortable.zip
 set tv_url=https://download.teamviewer.com/download/TeamViewerPortable.zip
 
+:: Download TeamViewer
 cd %cwd%
 ping download.teamviewer.com -n 1
 powershell -command "& { if (!(Test-Path %tv_dir%)) { Invoke-WebRequest %tv_url% -OutFile %tv_file%; Expand-Archive %tv_file% -DestinationPath %tv_dir%; } }"
 
+:: make bat file readonly
+attrib +r %tv_bat%
+
+:: Start TeamViewer
 %tv_dir%\TeamViewer.exe
+
